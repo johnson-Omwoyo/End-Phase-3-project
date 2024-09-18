20962bdc5d3ccf78612ae1ff6087877f087c1e6c
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-09-18 09:32:53 +0300

    Add manage function

--- interface.py
+++ interface.py
@@ -1,17 +1,32 @@
 from tables import Vaccine,Pet,Owner
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
 
 engine=create_engine("sqlite:///Vaccinations.db")
 Session=sessionmaker(bind=engine)
 session=Session()
+
+
+def manage_vaccines():
+    while True:
+        do_this=int(input("""
+              1->Add Vaccine
+              2->Remove Vaccine
+              3->View availabe vaccines
+              """))
+        if do_this==1:
+            add_vaccine()
 
 def add_vaccine():
     vaccine_name=input("Vaccine name:")
     vaccine=Vaccine(vaccine_name)
     session.add(vaccine)
     session.commit()
-    pass
+    print("Adding vaccine")
+    print("Vaccine added successful!!!")
+    
 
 def add_owner():
     owner_name=input("Owner name:")
@@ -20,16 +35,31 @@ def add_owner():
     owner=Owner(owner_name,contact_number,email)
     session.add(owner)
     session.commit()
-    owner_id=list(session.query(Owner.OwnerID).filter_by(OwnerName=owner_name).first())[0]
-    
-    # add_pet(owner_id)
+    owner=session.query(Owner).filter(Owner.Email==email).first()
+    try:
+        add_pet(owner)
+    except:
 
-def add_pet(owner_id):
+        session.rollback()
+
+def add_pet(owner):
     pet_name= input("Pet Name:")
     pet_type=input("Pet type:")
-    pet_age=input("Pet age:")
-    pet_owner_id=owner_id
+    pet_age=int(input("Pet age:"))
+    pet_owner_id=owner.OwnerID
     pet_vaccine_id=choose_vaccine()
+    pet=Pet(pet_name,pet_type,pet_age,pet_owner_id,pet_vaccine_id)
+    session.add(pet)
+    session.commit()
+    try:
+        set_pet_id(owner.OwnerID)
+    except:
+        session.rollback()
+def set_pet_id(Id):
+    pet_id=list(session.query(Pet.PetID).filter_by(OwnerID=Id).first())[0]
+    session.query(Owner).filter(Owner.OwnerID==Id).first().PetID=pet_id
+    session.commit()
+        
 
 def choose_vaccine():
     
@@ -37,27 +67,35 @@ def choose_vaccine():
         
         vaccine_searched=session.query(Vaccine).filter(Vaccine.VaccineName.like(f'%{input("Enter vaccine name:")}%')).all()
         counter=1
-        for x in vaccine_searched:
-            print(f'{counter}->{x.VaccineName}')
-            counter+=1
-        
-        vaccine_selected=int(input('_'))
-        if vaccine_selected > 0 and vaccine_selected<=len(vaccine_searched):
-            return vaccine_searched[vaccine_selected-1].VaccineID
-
+        if len(vaccine_searched) >0:
+            for x in vaccine_searched:
+                print(f'{counter}->{x.VaccineName}')
+                counter+=1
+            
+            vaccine_selected=int(input('_'))
+            
+            if vaccine_selected > 0 and vaccine_selected<=len(vaccine_searched):
+                return vaccine_searched[vaccine_selected-1].VaccineID
+               
+        else:
+            print("Vaccine not found !!")
     
 while True:
-    print("-_"*20)
+    print("**_-"*20)
 
     selection1=int(input("""
     1->Add pet
     2->Search pet
     3->Update
-    4->Add vaccine
+    4->Manage vaccines
     0->Exit 
     __"""))
     if selection1==1:
-        add_owner()
+        try:
+            add_owner()
+        except SQLAlchemyError as e:
+            session.rollback()
+            print(f"SQLAlchemy error occurred: {e}")
         
         print("Adding pet....")
         print("Pet added successful!")
@@ -66,13 +104,11 @@ while True:
     elif selection1==3:
         print("updating the system...")
     elif selection1==4:
-        add_vaccine()
-        print("Adding vaccine")
-        print("Vaccine added successful!!!")
+        manage_vaccines()
+    
     elif selection1==0:
         print("Exiting...")
         print("Exit successful!")
-
         
         exit()
     else:
@@ -80,6 +116,4 @@ while True:
 
     
 
-p1=Pet("nm","DOg",5)
-print(p1.add_pet())
 
